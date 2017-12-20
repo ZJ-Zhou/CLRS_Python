f628e5b90c9c6fd86cfb3fd9a8439433648fc17f
ZhouZeji <zhouzeji@gennlife.com>
2017-12-20 15:00:01 +0800

    More pythonic

--- sorting/insert_sorting.py
+++ sorting/insert_sorting.py
@@ -1,11 +1,9 @@
 def insert_sorting(arr):
     for i in range(1, len(arr)):
         j = 1
-        tmp = arr[i]
         while (arr[i] < arr[i - j] and i - j >= 0):
-            arr[i - j + 1] = arr[i - j]
+            arr[i - j + 1], arr[i - j] = arr[i - j], arr[i - j + 1]
             j += 1
-        arr[i - j + 1] = tmp
     return arr
 
 
